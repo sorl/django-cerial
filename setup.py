85aa4a9fa261157efad0ee6ce7b38de860a2a4e5
Mikko Hellsing <mikko@aino.se>
2011-08-09 15:54:08 +0200

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-cerial',
-    version='0.0.2',
+    version='0.0.3',
     description='',
     long_description=open('README.rst').read(),
     author='Mikko Hellsing',
